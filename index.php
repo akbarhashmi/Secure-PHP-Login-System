a1e39ece82db196647878f23c77ed21edf9cefbb
akbarhashmi <akbar6127@gmail.com>
2018-01-28 15:04:34 +0530

    Setting up the basics 0.1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h3>Creating html form</h3> 
+
+
+<p> login</p>
+
+	<form action="process.php" method="POST">
+
+		Username: <input type="text" name="username" placeholder="Enter"> <br><br>
+		
+		<input type="submit" name="button" value="Click here">
+
+
+
+
+</body>
+</html>
