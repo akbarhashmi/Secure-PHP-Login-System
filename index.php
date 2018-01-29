3cee91f9d15ed21bc7f8e1ffed1f4b917c971775
akbarhashmi <akbar6127@gmail.com>
2018-01-29 23:21:26 +0530

    Testing regular expressions with JavaScript 0.2

--- index.php
+++ index.php
@@ -4,17 +4,29 @@
 	<title></title>
 </head>
 <body>
+	<script>
+		function formValidation(){
+			var user = document.form1.username.value;
+			var reg = /^([A-Za-z0-9_])$/;
+			if(!user.match(reg)); 
+				alert('Invalid username');
+				return false;
+			}
+			return true;
+		}		
+	</script>
 
 <h3>Creating html form</h3> 
 
 
 <p> login</p>
 
-	<form action="process.php" method="POST">
-
-		Username: <input type="text" name="username" placeholder="Enter"> <br><br>
-		
+	<form name = "form1" onsubmit="return formValidation() " action="process.php" method="POST" >
+<!-- 	Order matters, first JS script run then the next php page visited -->
+ 		Username: <input type="text" name="username" placeholder="Enter" value="" > <br><br>		
 		<input type="submit" name="button" value="Click here">
+
+	</form>
 
 
 
--- process.php
+++ process.php
@@ -1,22 +1,17 @@
-
 <?php
     error_reporting(E_ALL); ini_set('display_errors', 1);
 define('DB_NAME','Database1');
 define('DB_USER','root');
 define('DB_PASSWORD','root');
 define('DB_HOST','localhost');
-
 $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD, DB_NAME);
 $db_selected = mysqli_select_db( $link, DB_NAME);
-
 if(!$link){
     die('error'. mysql_error());
 }
-
 if(!$db_selected){
     die('error' . mysql_error());
 }
-
 if(isset($_POST['button'])){
 	$value = $_POST['username'];
 	$sql= "INSERT INTO Demo (username) VALUES ('$value')";
@@ -25,12 +20,9 @@ else{
 	echo('Please login again');
 	exit();
 }
-
 if (!mysqli_query($link,$sql)){
         die('error' . mysql_error());
-
 }
-
 mysqli_close($link);
 echo "Hello Wqworld";
 ?>
