97e7e665ae2b61f6e592560ab32ab4e786cd771e
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-06 17:03:56 -0500

    Create InvalidArgumentException.php

---
+++ src/engine/Exception/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner/Developer)           <me@akbarhashmi.com>.
+ * @author Nicholas English (Contributor/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Akbarhashmi\Engine\Exception;
+
+/**
+ * InvalidArgumentException.
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
