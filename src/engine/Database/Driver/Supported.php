8d43ddea825ac94c8126c650e40b52bb28096695
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-09 03:20:18 -0500

    Update Supported.php

--- src/engine/Database/Driver/Supported.php
+++ src/engine/Database/Driver/Supported.php
@@ -1,1 +1,30 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner/Developer)            <me@akbarhashmi.com>.
+ * @author Nicholas English (Collaborator/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Akbarhashmi\Engine\Database\Driver;
 
+/**
+ * Supported.
+ */
+class Supported
+{
+    
+    /**
+     * @var array $supported The list of supported database drivers.
+     */
+    protected $supported = [
+        'mysql',
+        'pgsql',
+        'oracle'
+    ];
+  
+}
