f887f8efd5c148093c531134261d6a1654d6404b
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-12 15:49:31 -0500

    Update SessionTest.php

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -1,1 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner/Developer)           <me@akbarhashmi.com>.
+ * @author Nicholas English (Collaborator/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Akbarhashmi\Engine;
 
+use PHPUnit\Framework\TestCase;
+
+/**
+ * SessionTest.
+ */
+final class SessionTest extends TestCase
+{
+}
